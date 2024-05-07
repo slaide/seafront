b6e8463fd7d8c92ae047b08608cf04c951ad020e
Patrick Hennig <patrick_hennig@fastmail.com>
2024-05-07 15:57:07 +0200

    added link to documentation for sections of this software

---
+++ src/docLinks.js
@@ -0,0 +1,32 @@
+let docBaseLink="https://agate-erica-c9f.notion.site/SQUID-Microscope-Overview-9e3f4750ddc34e8b8641c23c456ae771"
+let docLinks={
+    /*
+        the links in comments are the only way to get links to blocks on a notion page, but those require login
+
+        the docBaseLink points to a publicly accessible page, and the hash target works for both (public and login-required)
+        pages
+    */
+   
+    // https://www.notion.so/SQUID-Microscope-Overview-9e3f4750ddc34e8b8641c23c456ae771?pvs=4#5588d1ccd2554ceea0e569fa64539134
+    mainCameraTriggerAndPixelFormat:docBaseLink+"#5588d1ccd2554ceea0e569fa64539134",
+    // https://www.notion.so/SQUID-Microscope-Overview-9e3f4750ddc34e8b8641c23c456ae771?pvs=4#6b2a466d1bfb4e32b8bb10c6ce81dd34
+    storageOutputPath:docBaseLink+"#6b2a466d1bfb4e32b8bb10c6ce81dd34",
+    // https://www.notion.so/SQUID-Microscope-Overview-9e3f4750ddc34e8b8641c23c456ae771?pvs=4#eea5cd39d0754ba0b7c9b7b479ed0d76
+    siteConfig:docBaseLink+"#eea5cd39d0754ba0b7c9b7b479ed0d76",
+    // https://www.notion.so/SQUID-Microscope-Overview-9e3f4750ddc34e8b8641c23c456ae771?pvs=4#50037e2e07af443f8770dd43b1883e22
+    mainAcquisitionControl:docBaseLink+"#50037e2e07af443f8770dd43b1883e22",
+    // https://www.notion.so/SQUID-Microscope-Overview-9e3f4750ddc34e8b8641c23c456ae771?pvs=4#b84e4ea4068843208a81e6c87f0a04b3
+    plateWellSelection:docBaseLink+"#b84e4ea4068843208a81e6c87f0a04b3",
+    // https://www.notion.so/SQUID-Microscope-Overview-9e3f4750ddc34e8b8641c23c456ae771?pvs=4#2aa84a1f618448a09a50de21781101c0
+    snapSelectionControl:docBaseLink+"#2aa84a1f618448a09a50de21781101c0",
+    // https://www.notion.so/SQUID-Microscope-Overview-9e3f4750ddc34e8b8641c23c456ae771?pvs=4#6144312562554d0391252a76c5c9e11f
+    imagingChannelConfig:docBaseLink+"#6144312562554d0391252a76c5c9e11f",
+    // https://www.notion.so/SQUID-Microscope-Overview-9e3f4750ddc34e8b8641c23c456ae771?pvs=4#ae03d33b2159475da3bdfd0deb6b54cb
+    histogramPanel:docBaseLink+"#ae03d33b2159475da3bdfd0deb6b54cb",
+    // https://www.notion.so/SQUID-Microscope-Overview-9e3f4750ddc34e8b8641c23c456ae771?pvs=4#5d7b1f71b64a438d972620fcee32f83d
+    objectivePositionControl:docBaseLink+"#5d7b1f71b64a438d972620fcee32f83d",
+    // https://www.notion.so/SQUID-Microscope-Overview-9e3f4750ddc34e8b8641c23c456ae771?pvs=4#55a36b2c6bae4d6f9fb6640961eaa09c
+    laserReflectionAutofocusControl:docBaseLink+"#55a36b2c6bae4d6f9fb6640961eaa09c",
+    // https://www.notion.so/SQUID-Microscope-Overview-9e3f4750ddc34e8b8641c23c456ae771?pvs=4#561a851d9be6487bb6c3fa057096e259
+    softwareAutofocusControl:docBaseLink+"#561a851d9be6487bb6c3fa057096e259",
+}
