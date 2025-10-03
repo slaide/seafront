4ff825f3cfd923aa89b31116f9240db8814a84a8
Patrick Hennig <patrick_hennig@fastmail.com>
2025-10-03 11:14:39 +0200

    some fixes in utility install script

--- install/related_utilities.sh
+++ install/related_utilities.sh
@@ -1,17 +1,30 @@
+# install into separate venvs:
+# - orange
+# - cellprofiler
+# - cellprofiler analyst
+
+# requires uv to be already installed
+
+set -euo pipefail
+
 # good to have
-sudo apt install gcc clang build-essential cmake make
+sudo apt install -fy gcc clang build-essential cmake make
 # for a user
-sudo apt install tree curl wget micro
+sudo apt install -fy tree curl wget micro
+# for orange
+sudo apt install -fy libxcb-xinerama0
 # for cellprofiler
-sudo apt install python3-dev openjdk-11-jdk-headless default-libmysqlclient-dev libsdl2-dev
+sudo apt install -fy python3-dev openjdk-11-jdk-headless default-libmysqlclient-dev libsdl2-dev
 
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 
-# install uv
 uv venv venv_orange --python 3.12
 source venv_orange/bin/activate
+uv pip install PyQt5 PyQtWebEngine
 uv pip install orange3
-# run with python -m Orange.canvas
+# run with
+# source venv_orange/bin/activate
+# python -m Orange.canvas
 deactivate
 
 wget https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-24.04/wxpython-4.2.3-cp39-cp39-linux_x86_64.whl
@@ -21,22 +34,28 @@ uv pip install --upgrade setuptools wheel
 uv pip install wxpython-4.2.3-cp39-cp39-linux_x86_64.whl
 rm wxpython-4.2.3-cp39-cp39-linux_x86_64.whl
 uv pip install cellprofiler==4.2.8
-# run with python -m cellprofiler
+# run with
+# export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+# source venv_cellprofiler/bin/activate
+# python3 -m cellprofiler
 deactivate
 
-
 wget https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-22.04/wxPython-4.2.1-cp38-cp38-linux_x86_64.whl
-uv venv venv_cellprofileranalys --python 3.8
-source venv_cellprofiler/bin/activate
+uv venv venv_cellprofileranalyst --python 3.8
+source venv_cellprofileranalyst/bin/activate
 uv pip install --upgrade setuptools wheel numpy
 uv pip install --no-build-isolation pandas seaborn scikit-learn verlib python-javabridge python-bioformats
 uv pip install wxPython-4.2.1-cp38-cp38-linux_x86_64.whl
 rm wxPython-4.2.1-cp38-cp38-linux_x86_64.whl
 wget https://github.com/CellProfiler/CellProfiler-Analyst/archive/refs/tags/3.0.5.tar.gz
+mv 3.0.5.tar.gz CellProfiler-Analyst-3.0.5.tar.gz
 tar -xzf CellProfiler-Analyst-3.0.5.tar.gz
 uv pip install --no-build-isolation ./CellProfiler-Analyst-3.0.5
 rm CellProfiler-Analyst-3.0.5.tar.gz
-ln -s /usr/lib/x86_64-linux-gnu/libtiff.so.6 /usr/lib/x86_64-linux-gnu/libtiff.so.5
-# do NOT delete CellProfiler-Analyst-3.0.5
-# run with python CellProfiler-Analyst-3.0.5/CellProfiler-Analyst.py
+# unsafe, but seems to work
+sudo ln -s /usr/lib/x86_64-linux-gnu/libtiff.so.6 /usr/lib/x86_64-linux-gnu/libtiff.so.5
+# do NOT delete CellProfiler-Analyst-3.0.5 (the folder)
+# run with
+# source venv_cellprofileranalyst/bin/activate
+# python CellProfiler-Analyst-3.0.5/CellProfiler-Analyst.py
 deactivate
